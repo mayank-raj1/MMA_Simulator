122abadb7ac6340fd7fd2d4dac99f7ed86a94947
Mayank Raj <rajmay@sheridancollege.ca>
2023-06-19 10:23:15 -0400

    Changes made to Match class and fighter class; comments code clean up

--- src/Fighter.java
+++ src/Fighter.java
@@ -1,12 +1,16 @@
 import java.util.HashMap;
-import java.util.List;
+import java.util.Map;
 
+/**
+ * Represents a fighter in a match.
+ */
 public class Fighter {
 	private static int baseFighterId = 1000;
-	public enum WeightClass{LightWeight, MiddleWeight, HeavyWeight}
-	public enum Status{Available, InMatch}
-	private String name;
-	private String id;
+	public enum WeightClass {LightWeight, MiddleWeight, HeavyWeight}
+	public enum Status {Available, InMatch}
+
+	private final String name;
+	private final String id;
 	private WeightClass weightClass;
 	private double strength;
 	private double speed;
@@ -14,9 +18,22 @@ public class Fighter {
 	private int age;
 	private double weight;
 	private Status status;
-	{status = Status.Available;}
-	private HashMap<String, Record> records;
-	public Fighter(String name, double strength, double speed, double skill, int age, double weight, int id){
+	private Map<String, Record> records;
+
+	{ status = Status.Available; }
+
+	/**
+	 * Creates a fighter with the specified attributes and ID.
+	 *
+	 * @param name     the name of the fighter
+	 * @param strength the strength of the fighter (0.0 - 10.0 range)
+	 * @param speed    the speed of the fighter (0.0 - 10.0 range)
+	 * @param skill    the skill of the fighter (0.0 - 10.0 range)
+	 * @param age      the age of the fighter (18 - 100 range)
+	 * @param weight   the weight of the fighter (40.0 - 300.0 range)
+	 * @param id       the ID of the fighter
+	 */
+	public Fighter(String name, double strength, double speed, double skill, int age, double weight, int id) {
 		this.name = name;
 		this.setStrength(strength);
 		this.setSpeed(speed);
@@ -24,104 +41,218 @@ public class Fighter {
 		this.setAge(age);
 		this.setWeight(weight);
 		this.id = Integer.toString(id);
+		this.records = new HashMap<>();
 	}
 
-	public Fighter(String name, double strength, double speed, double skill, int age, double weight){
-		this.name = name;
-		this.setStrength(strength);
-		this.setSpeed(speed);
-		this.setSkill(skill);
-		this.setAge(age);
-		this.setWeight(weight);
+	/**
+	 * Creates a fighter with the specified attributes and automatically generated ID.
+	 *
+	 * @param name     the name of the fighter
+	 * @param strength the strength of the fighter (0.0 - 10.0 range)
+	 * @param speed    the speed of the fighter (0.0 - 10.0 range)
+	 * @param skill    the skill of the fighter (0.0 - 10.0 range)
+	 * @param age      the age of the fighter (18 - 100 range)
+	 * @param weight   the weight of the fighter (40.0 - 300.0 range)
+	 */
+	public Fighter(String name, double strength, double speed, double skill, int age, double weight) {
+		this(name, strength, speed, skill, age, weight, baseFighterId++);
 	}
+
+	// Getter and setter methods
+
+	/**
+	 * Retrieves the name of the fighter.
+	 *
+	 * @return the name of the fighter
+	 */
 	public String getName() {
 		return this.name;
 	}
-	public void setName(String name) {
-		this.name = name;
-	}
 
+	/**
+	 * Retrieves the weight class of the fighter.
+	 *
+	 * @return the weight class of the fighter
+	 */
 	public WeightClass getWeightClass() {
 		return this.weightClass;
 	}
 
+	/**
+	 * Retrieves the strength of the fighter.
+	 *
+	 * @return the strength of the fighter
+	 */
 	public double getStrength() {
 		return this.strength;
 	}
 
-	public void setStrength(double strength) throws IllegalArgumentException{
-		if (strength>10.0||strength<0){
-			throw new IllegalArgumentException("Give the strength in a 0-10.0 range!");
-		}
+	/**
+	 * Sets the strength of the fighter.
+	 *
+	 * @param strength the strength of the fighter (0.0 - 10.0 range)
+	 * @throws IllegalArgumentException if the strength value is outside the valid range
+	 */
+	public void setStrength(double strength) throws IllegalArgumentException {
+		validateAttributeRange(strength, "strength", 0.0, 10.0);
 		this.strength = strength;
 	}
 
+	/**
+	 * Retrieves the speed of the fighter.
+	 *
+	 * @return the speed of the fighter
+	 */
 	public double getSpeed() {
 		return this.speed;
 	}
 
-	public void setSpeed(double speed) throws IllegalArgumentException{
-		if (speed>10.0||speed<0){
-			throw new IllegalArgumentException("Give the speed in a 0-10.0 range!");
-		}
+	/**
+	 * Sets the speed of the fighter.
+	 *
+	 * @param speed the speed of the fighter (0.0 - 10.0 range)
+	 * @throws IllegalArgumentException if the speed value is outside the valid range
+	 */
+	public void setSpeed(double speed) throws IllegalArgumentException {
+		validateAttributeRange(speed, "speed", 0.0, 10.0);
 		this.speed = speed;
 	}
 
+	/**
+	 * Retrieves the skill of the fighter.
+	 *
+	 * @return the skill of the fighter
+	 */
 	public double getSkill() {
 		return this.skill;
 	}
+
+	/**
+	 * Sets the skill of the fighter.
+	 *
+	 * @param skill the skill of the fighter (0.0 - 10.0 range)
+	 * @throws IllegalArgumentException if the skill value is outside the valid range
+	 */
 	public void setSkill(double skill) throws IllegalArgumentException {
-		if (skill>10.0||skill<0){
-			throw new IllegalArgumentException("Give the skill in a 0-10.0 range!");
-		}
+		validateAttributeRange(skill, "skill", 0.0, 10.0);
 		this.skill = skill;
 	}
 
+	/**
+	 * Retrieves the age of the fighter.
+	 *
+	 * @return the age of the fighter
+	 */
 	public int getAge() {
 		return this.age;
 	}
-	public void setAge(int age) throws IllegalArgumentException {
-		if (age<18||age>100){
-			throw new IllegalArgumentException("Enter the age in valid range 18-100");
-		}
-		this.age = age;
+
+	/**
+	 * Adds to the  age of the fighter.
+	 */
+
+	public void addAge(){
+		this.age++;
 	}
 
+	/**
+	 * Retrieves the weight of the fighter.
+	 *
+	 * @return the weight of the fighter
+	 */
 	public double getWeight() {
 		return weight;
 	}
 
-	public void setWeight(double weight) throws IllegalArgumentException{
-		if (weight<40.0||weight>300){
-			throw new IllegalArgumentException("Enter an reasonable weight for the fighter");
-		}
+	/**
+	 * Sets the weight of the fighter.
+	 *
+	 * @param weight the weight of the fighter (40.0 - 300.0 range)
+	 * @throws IllegalArgumentException if the weight value is outside the valid range
+	 */
+	public void setWeight(double weight) throws IllegalArgumentException {
+		validateAttributeRange(weight, "weight", 40.0, 300.0);
 		this.weight = weight;
 		setWeightClass();
 	}
-	private void setWeightClass(){
-		if (this.weight<70){
-			this.weightClass = WeightClass.LightWeight;
-		} else if (this.weight<84) {
-			this.weightClass = WeightClass.MiddleWeight;
-		}
-		else {
-			this.weightClass = WeightClass.HeavyWeight;
-		}
-	}
 
+	/**
+	 * Retrieves the ID of the fighter.
+	 *
+	 * @return the ID of the fighter
+	 */
 	public String getId() {
 		return id;
 	}
 
-	public void inMatch(){this.status = Status.InMatch;}
+	/**
+	 * Sets the status of the fighter to "InMatch".
+	 */
+	public void inMatch() {
+		this.status = Status.InMatch;
+	}
 
+	/**
+	 * Retrieves the status of the fighter.
+	 *
+	 * @return the status of the fighter
+	 */
 	public Status getStatus() {
 		return status;
 	}
 
-	public void lost(){this.status = Status.Available;}
-	public void addRecord(Record record){
+	/**
+	 * Sets the status of the fighter to "Available".
+	 */
+	public void lost() {
+		this.status = Status.Available;
+	}
+
+	/**
+	 * Adds a record to the fighter's list of records.
+	 *
+	 * @param record the record to be added
+	 */
+	public void addRecord(Record record) {
 		this.records.put(record.match().getMatchId(), record);
+	}
+
+	/**
+	 * Retrieves a record of the fighter.
+	 *
+	 * @param index of the record in the map
+	 */
+	public Record getRecord(int index) {
+		return (Record) records.values().toArray()[index];
+	}
+	public Record getRecord(String matchId) {
+		return records.get(matchId);
+	}
+
+	// Private utility methods
+	private void setAge(int age) throws IllegalArgumentException {
+		validateAttributeRange(age);
+		this.age = age;
+	}
+	private void setWeightClass() {
+		if (this.weight < 70) {
+			this.weightClass = WeightClass.LightWeight;
+		} else if (this.weight < 84) {
+			this.weightClass = WeightClass.MiddleWeight;
+		} else {
+			this.weightClass = WeightClass.HeavyWeight;
+		}
+	}
+	private void validateAttributeRange(double value, String attributeName, double minValue, double maxValue) {
+		if (value < minValue || value > maxValue) {
+			throw new IllegalArgumentException("Invalid value for " + attributeName + ". Value should be in range: " + minValue + " - " + maxValue);
+		}
+	}
+
+	private void validateAttributeRange(int value) {
+		if (value < 18 || value > 100) {
+			throw new IllegalArgumentException("Invalid value for " + "age" + ". Value should be in range: " + 18 + " - " + 100);
+		}
 	}
 
 }
--- src/Match.java
+++ src/Match.java
@@ -1,3 +1,8 @@
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Represents a match between two fighters.
+ */
 public class Match {
     private static int baseMatchId = 1000;
     private final String matchId;
@@ -5,79 +10,142 @@ public class Match {
     private Fighter fighter2;
     private Record record;
 
-    public Match(Fighter fighter1, Fighter fighter2) {
+    /**
+     * Constructs a Match object with the provided fighters and a prefix for generating a unique match ID.
+     *
+     * @param fighter1 The first fighter.
+     * @param fighter2 The second fighter.
+     * @param prefix   The prefix for generating a unique match ID.
+     */
+    public Match(Fighter fighter1, Fighter fighter2, String prefix) {
         this.setFighter1(fighter1);
         this.setFighter2(fighter2);
-        matchId = Integer.toString(baseMatchId++);
+        matchId = prefix + (baseMatchId++); // Generates a unique match ID by appending the prefix to baseMatchId
     }
 
-    public Match(Fighter fighter1, Fighter fighter2, int matchId) {
+    /**
+     * Constructs a Match object with the provided fighters and a match ID.
+     *
+     * @param fighter1 The first fighter.
+     * @param fighter2 The second fighter.
+     * @param matchId  The match ID.
+     */
+    public Match(String matchId, Fighter fighter1, Fighter fighter2) {
         this.setFighter1(fighter1);
         this.setFighter2(fighter2);
-        this.matchId = Integer.toString(matchId);
+        this.matchId = matchId; // Sets the match ID using the provided integer value
     }
 
+    /**
+     * Constructs a Match object with a prefix for generating a unique match ID.
+     *
+     * @param prefix   The prefix for generating a unique match ID.
+     */
+    public Match(String prefix) {
+        this.matchId = prefix + baseMatchId++; // Sets the match ID using the provided integer value
+    }
+
+    /**
+     * Returns the match ID.
+     *
+     * @return The match ID.
+     */
     public String getMatchId() {
-        return this.matchId;
+        return matchId;
     }
 
+    /**
+     * Returns the first fighter.
+     *
+     * @return The first fighter.
+     */
     public Fighter getFighter1() {
-        return this.fighter1;
+        return fighter1;
     }
 
-    public void setFighter1(Fighter fighter1) throws IllegalArgumentException {
-        if (!(fighter1.getStatus() == Fighter.Status.Available)) {
-            throw new IllegalArgumentException("The Fighter should be available");
-        }
+    /**
+     * Sets the first fighter.
+     *
+     * @param fighter1 The first fighter to set.
+     */
+    public void setFighter1(Fighter fighter1) {
+        validateAvailability(fighter1); // Validates fighter availability
         this.fighter1 = fighter1;
-        fighter1.inMatch();
+        fighter1.inMatch(); // Sets the first fighter and updates their status
     }
 
+    /**
+     * Returns the second fighter.
+     *
+     * @return The second fighter.
+     */
     public Fighter getFighter2() {
-        return this.fighter2;
+        return fighter2;
     }
 
+    /**
+     * Sets the second fighter.
+     *
+     * @param fighter2 The second fighter to set.
+     */
     public void setFighter2(Fighter fighter2) {
-        if (!(fighter2.getStatus() == Fighter.Status.Available)) {
-            throw new IllegalArgumentException("The Fighter should be available");
-        }
+        validateAvailability(fighter2); // Validates fighter availability
         this.fighter2 = fighter2;
-        fighter2.inMatch();
+        fighter2.inMatch(); // Sets the second fighter and updates their status
     }
 
+    /**
+     * Performs the fight between the two fighters, calculates the scores, and records the match result.
+     */
     public void fight() {
-        int[] score = {0, 0};
+        int[] score = calculateScores(); // Calculates the scores for each fighter
 
-        if (fighter1.getSkill() > fighter2.getSkill()) {
-            score[0]++;
-        } else if (fighter1.getSkill() == fighter2.getSkill()) {
-            score[0]++;
-            score[1]++;
-        } else {
-            score[1]++;
-        }
-        if (fighter1.getStrength() > fighter2.getStrength()) {
-            score[0]++;
-        } else if (fighter1.getStrength() == fighter2.getStrength()) {
-            score[0]++;
-            score[1]++;
-        } else {
-            score[1]++;
-        }
-        if (fighter1.getSpeed() > fighter2.getSpeed()) {
-            score[0]++;
-        } else if (fighter1.getSpeed() == fighter2.getSpeed()) {
-            score[0]++;
-            score[1]++;
-        } else {
-            score[1]++;
-        }
-        this.record = new Record(this, (score[0] > score[1] ? fighter1 : fighter2), score);
-        fighter1.addRecord(record);
-        fighter2.addRecord(record);
+        this.record = new Record(this, (score[0] > score[1] ? fighter1 : fighter2), score); // Creates a new record for the match with the winner and scores
+
+        fighter1.addRecord(record); // Adds the record to the fighter's list of records
+        fighter2.addRecord(record); // Adds the record to the fighter's list of records
     }
 
+    /**
+     * Returns the results of the match including the winner's name and scores.
+     *
+     * @return The match results.
+     */
     public String getResults() {
-        return String.format("%S won, score[ %d : %d] ", this.record.winner().getName(), this.record.score()[0], this.record.score()[1]);
+        return String.format("%S won, score[ %d : %d] ", record.winner().getName(), record.score()[0], record.score()[1]);
+    }
+
+    //private util methods
+    private void validateAvailability(@NotNull Fighter fighter) {
+        if (fighter.getStatus() != Fighter.Status.Available) {
+            throw new IllegalArgumentException("The Fighter should be available"); // Throws an exception if the fighter is not available
+        }
+    }
+
+    private int[] calculateScores() {
+        int[] score = new int[2];
+
+        score[0] += compareSkills(fighter1, fighter2);
+        score[1] += compareSkills(fighter2, fighter1);
+
+        score[0] += compareStrengths(fighter1, fighter2);
+        score[1] += compareStrengths(fighter2, fighter1);
+
+        score[0] += compareSpeeds(fighter1, fighter2);
+        score[1] += compareSpeeds(fighter2, fighter1);
+
+        return score;
+    }
+
+    private double compareSkills(@NotNull Fighter fighter1, @NotNull Fighter fighter2) {
+        return Double.compare(fighter1.getSkill(), fighter2.getSkill());
+    }
+
+    private double compareStrengths(@NotNull Fighter fighter1, @NotNull Fighter fighter2) {
+        return Double.compare(fighter1.getStrength(), fighter2.getStrength());
+    }
+
+    private double compareSpeeds(@NotNull Fighter fighter1, @NotNull Fighter fighter2) {
+        return Double.compare(fighter1.getSpeed(), fighter2.getSpeed());
     }
 }
